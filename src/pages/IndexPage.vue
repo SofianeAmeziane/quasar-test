529e0c0fc3ccafd3b85530e3f5074c0f9307b5f0
sofianeameziane <sofianeameziane2@gmail.com>
2023-09-25 11:22:14 -0400

    test

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="flex flex-center text-grey">
-    <div>
-      <div class="text-h2 text-bold q-mb-md">Welcome Home</div>
-      <div class="text-h6 q-mb-md">
-        this is my first application to practice my knowledges on vue js using
-        Quasar framework
+    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <div class="absolute-full text-subtitle2 flex flex-center">
+        <div>
+          <div class="text-h2 text-bold q-mb-md">Welcome Home</div>
+          <div class="text-h6 q-mb-md">
+            this is my first application to practice my knowledges on vue js
+            using Quasar framework.
+          </div>
+        </div>
       </div>
-      <img
-        alt="Quasar logo"
-        src="~assets/home-img.jpg"
-        style="width: 200px; height: 200px"
-      />
-    </div>
+    </q-img>
   </q-page>
 </template>
 
